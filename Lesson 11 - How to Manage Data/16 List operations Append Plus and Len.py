4729f9cf95a3e219bb48efb52c2cc22504af5822
Aakanksha <aaks2492@gmail.com>
2020-03-22 09:09:17 -0700

    Solution for Append Plus and Len sum

---
+++ Lesson 11 - How to Manage Data/16 List operations Append Plus and Len.py
@@ -0,0 +1,4 @@
+p=[1,2]
+p.append(3) # append adds element at end of list
+p=p+[4,5] #+ operates as concatenation of strings, doesnt mutate existing list ,assigns new list consisting of all elements concatenated together
+print len(p) # len provides length of list beginning from 1
