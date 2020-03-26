70730a1f140d2de3de41d7156991915f598a97f7
Aakanksha <aaks2492@gmail.com>
2020-03-26 14:20:47 -0700

    Solution to Measure Udacity sum

---
+++ Lesson 11 - How to Manage Data/25 Measure Udacity.py
@@ -0,0 +1,19 @@
+# Define a procedure, measure_udacity,
+# that takes as its input a list of strings,
+# and returns a number that is a count
+# of the number of elements in the input
+# list that start with the uppercase
+# letter 'U'.
+
+def measure_udacity(p):
+    sum=0
+    for i in p:
+        if i.find('U')==0: # i[0]=='U'
+            sum=sum+1
+    return sum
+
+print measure_udacity(['Dave','Sebastian','Katy'])
+#>>> 0
+
+print measure_udacity(['Umika','Umberto'])
+#>>> 2
