f6413081bb32d0ca92b054a3d0495bf2be1c31a5
Aakanksha <aaks2492@gmail.com>
2020-04-22 05:25:03 -0700

    Solution to Split message sum as per Course

---
+++ Lesson 15 - Responding to Queries/5 Split message Course.py
@@ -0,0 +1,8 @@
+quote = "In washington, it's dog eat dog. In academia, it's exactly the opposite. --- Robert Reich"
+print quote.split()
+quote = """
+            There's no business like show business,
+            but there are several businesses like accounting.
+            (David Letterman)
+        """
+print quote.split()
